72cbba783e0981f1195e16f88259d8571d632563
Svinci131 <svinci131@gmail.com>
2015-12-29 12:34:42 -0500

    made download icon into anchor with download attr- removed download.js file

--- javascript/download.js
+++ javascript/download.js
@@ -1,6 +1,9 @@
 $(".thumbnails").on ('click', '.download', function() {
+  return;
 	var id = $( this ).parents('.photo').find('img').attr("id");
 	var link = $( this ).parents('.photo').find('img').attr("src");
+  var self = $( this );
+
 	// var url = $( this ).parents('.photo').find('img').attr("src")
  	var xhr = new XMLHttpRequest();
     xhr.responseType = 'blob';
@@ -8,7 +11,7 @@ $(".thumbnails").on ('click', '.download', function() {
       var reader  = new FileReader();
       reader.onloadend = function () {
           console.log(reader.result);
-          downloadURI(reader.result, id+".jpg");
+          downloadURI(reader.result, id+".jpg", self);
           //link = reader.result;
           //var url = link.replace(/^data:image\/[^;]/, 'data:application/octet-stream');
 		  //window.open(url);
@@ -19,10 +22,16 @@ $(".thumbnails").on ('click', '.download', function() {
     xhr.send();
 });
 
-function downloadURI(uri, name) {
+function downloadURI(uri, name, self) {
     var link = document.createElement("a");
-    link.download = name;
-    link.href = uri;
-    link.click();
+    link.innerHTML = name;
+    link.setAttribute('download', name)
+    link.setAttribute('href', uri);
+    // link.style.visibility = 'hidden';
+    // link.style.height = '0px';
+    link.style.display = 'block';
+    self.parents('.thumbnail_label').append( link );
+    $(self).parents('.thumbnail_label').find('a').click();
+        console.log( self, link, $( self ).parents('.thumbnail_label').find('a') )
 }
 
